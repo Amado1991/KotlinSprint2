5e7b4d63a93c09ca38f0a1b41e5349734daad6fb
Amado1991 <michab1991@gmail.com>
2024-10-11 12:18:34 +0300

    Initial Commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,12 @@
+package org.example
+
+
+fun main() {
+
+    val numberOfOrders: Int = 75
+    println(numberOfOrders)
+
+    val thanksForOrder: String = "Благодарим за покупку"
+    println(thanksForOrder)
+
+    }

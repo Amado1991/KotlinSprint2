5ba222f6029ce0e82c38c42f92ab5ba9a062bd97
Amado1991 <michab1991@gmail.com>
2024-10-11 17:14:11 +0300

    KS-1-5 Variables  Yurii Gagarin

--- src/main/kotlin/lesson_1/KS-1-5.kt
+++ src/main/kotlin/lesson_1/KS-1-5.kt
@@ -1,19 +1,16 @@
 package org.example.lesson_1
 
+const val SECONDS = 6480
+const val SECONDS_IN_HOUR = 3600
+const val MINUTES_IN_HOUR = 60
 
 fun main(){
 
-    val seconds = 6480
-
-    val hour: Int = seconds / 3600
-
-    val remainingSec: Int = seconds - hour * 3600
-
-    val minutes: Int = remainingSec / 60
-
-    val remainingSeconds: Int = remainingSec - minutes * 60
+    val hour: Int = SECONDS / SECONDS_IN_HOUR
+    val remainingSec: Int = SECONDS - hour * SECONDS_IN_HOUR
+    val minutes: Int = remainingSec / MINUTES_IN_HOUR
+    val remainingSeconds: Int = remainingSec - minutes * MINUTES_IN_HOUR
 
     println(String.format("%02d:%02d:%02d", hour, minutes, remainingSeconds))
-
 
 }
